f5c8856b4a7be7cabdee5d7c5df2c02eccc2c50d
Jeremias Simonetta <jeresimon86@gmail.com>
2024-03-28 15:37:00 -0300

    Primer prototipo

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import Navbar from "./components/Navbar.jsx";
+
 function App() {
-  return ( <h1>Este es el app.</h1>)
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+const Navbar = () => {
+  return (
+    <div>
+      <img
+        src="https://res.cloudinary.com/dptry1l5s/image/upload/v1711649917/Captura_de_pantalla_69_yxvzkl.png"
+        alt=""
+      />
+      <ul>
+        <li>Todas las categorías</li>
+        <li>Remeras</li>
+        <li>Buzos</li>
+        <li>Prints</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
